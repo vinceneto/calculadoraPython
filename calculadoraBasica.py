bef36406a18ceeb2c2a890d9e828c1d966dc2e23
vinceneto <vnetoxd@gmail.com>
2020-06-29 13:13:37 -0400

    Inicializando

---
+++ calculadoraBasica.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+menu_inicial = Tk()
+menu_inicial.title("Calculadora Básica")
+
+menu_inicial.geometry("500x250+200+200")
+menu_inicial.resizable(0, 0)
+
+menu_inicial.mainloop()
